ce39bc2661edbb63fd75fdf4d5aba6afff7ff0d4
Elly Landrum <ella3224@colorado.edu>
2022-02-19 12:42:53 -0700

    Starting Stats

--- js/processEquation.js
+++ js/processEquation.js
@@ -35,12 +35,12 @@ function processAnswer(answer, entered, prompt){
   var legal = true
   var blues = []
   var greens = []
-  var cleanAnswer = mathClean(answer)
-  legal = cleanAnswer[1]
-  cleanAnswer = cleanAnswer[0]
+  var cleanEntered = mathClean(entered)
+  legal = cleanEntered[1]
+  cleanEntered = cleanEntered[0]
   //Only proceed if the equation is legal math
   if(legal){
-    var simplified = math.rationalize(cleanAnswer).toString().replace(/\s/g, "")
+    var simplified = math.rationalize(cleanEntered).toString().replace(/\s/g, "")
     if (simplified === prompt){
       valid = true
     }
@@ -69,4 +69,3 @@ function processAnswer(answer, entered, prompt){
     };
   return dict
 }
-console.log(processAnswer("(x+1)^ - 1", "(x+1)^ - 1", "x^+2x"))
