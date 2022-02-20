a2338342881ab6a22b1cf0effbc0341bdf695c16
JosephineMartin <joma1927@colorado.edu>
2022-02-19 17:40:33 -0700

    images

---
+++ js/instructions.js
@@ -0,0 +1,8 @@
+export function displayInstructions(){
+  var instructions = document.getElementById("instructions_popup");
+  instructions.style.display = "block"
+}
+export function closeInstructions(){
+  var instructions = document.getElementById("instructions_popup");
+  instructions.style.display = "none"
+}
